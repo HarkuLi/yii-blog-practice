7896397662d6543e3ec13bfdca223946044081f6
HarkuLi <harku85697708@hotmail.com>
2018-01-16 14:19:20 +0800

    modify format to follow psr

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,10 +2,11 @@
 
 // This is the database connection configuration.
 return array(
-	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-	'connectionString' => 'mysql:host=localhost;dbname=blog',
-	// 'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '1234,Qwer',
-	// 'charset' => 'utf8',
+    // 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+    'connectionString' => 'mysql:host=localhost;dbname=blog',
+    // 'emulatePrepare' => true,
+    'username' => 'root',
+    'password' => '1234,Qwer',
+    // 'charset' => 'utf8',
+    'tablePrefix' => 'tbl_',
 );
--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -5,13 +5,13 @@
 
 $this->pageTitle=Yii::app()->name . ' - Contact Us';
 $this->breadcrumbs=array(
-	'Contact',
+    'Contact',
 );
 ?>
 
 <h1>Contact Us</h1>
 
-<?php if(Yii::app()->user->hasFlash('contact')): ?>
+<?php if (Yii::app()->user->hasFlash('contact')): ?>
 
 <div class="flash-success">
 	<?php echo Yii::app()->user->getFlash('contact'); ?>
@@ -26,11 +26,11 @@ If you have business inquiries or other questions, please fill out the following
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'contact-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
+    'id'=>'contact-form',
+    'enableClientValidation'=>true,
+    'clientOptions'=>array(
+        'validateOnSubmit'=>true,
+    ),
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
@@ -38,39 +38,39 @@ If you have business inquiries or other questions, please fill out the following
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name'); ?>
-		<?php echo $form->error($model,'name'); ?>
+		<?php echo $form->labelEx($model, 'name'); ?>
+		<?php echo $form->textField($model, 'name'); ?>
+		<?php echo $form->error($model, 'name'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email'); ?>
-		<?php echo $form->error($model,'email'); ?>
+		<?php echo $form->labelEx($model, 'email'); ?>
+		<?php echo $form->textField($model, 'email'); ?>
+		<?php echo $form->error($model, 'email'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'subject'); ?>
-		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'subject'); ?>
+		<?php echo $form->labelEx($model, 'subject'); ?>
+		<?php echo $form->textField($model, 'subject', array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model, 'subject'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'body'); ?>
-		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'body'); ?>
+		<?php echo $form->labelEx($model, 'body'); ?>
+		<?php echo $form->textArea($model, 'body', array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model, 'body'); ?>
 	</div>
 
-	<?php if(CCaptcha::checkRequirements()): ?>
+	<?php if (CCaptcha::checkRequirements()): ?>
 	<div class="row">
-		<?php echo $form->labelEx($model,'verifyCode'); ?>
+		<?php echo $form->labelEx($model, 'verifyCode'); ?>
 		<div>
 		<?php $this->widget('CCaptcha'); ?>
-		<?php echo $form->textField($model,'verifyCode'); ?>
+		<?php echo $form->textField($model, 'verifyCode'); ?>
 		</div>
 		<div class="hint">Please enter the letters as they are shown in the image above.
 		<br/>Letters are not case-sensitive.</div>
-		<?php echo $form->error($model,'verifyCode'); ?>
+		<?php echo $form->error($model, 'verifyCode'); ?>
 	</div>
 	<?php endif; ?>
 
