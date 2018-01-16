f2df0d1aad74999fbcae73f48f921cabbf7fd466
HarkuLi <harku85697708@hotmail.com>
2018-01-16 11:58:30 +0800

    first commit

---
+++ protected/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	'connectionString' => 'mysql:host=localhost;dbname=blog',
+	// 'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => '1234,Qwer',
+	// 'charset' => 'utf8',
+);
